36d35c102834b103a05b95084caf78aec9a6c8f2
huyhuy <huyhuy@gmail.com>
2026-03-16 22:39:48 +0700

    Update plugin load_newmap: SyncActionNode -> StatefulActionNode, Clear costmap before planner

--- real_robot_plugins/include/real_robot_plugins/load_new_map.hpp
+++ real_robot_plugins/include/real_robot_plugins/load_new_map.hpp
@@ -8,11 +8,12 @@
 #include "nav2_msgs/srv/load_map.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.h"
-
+#include <future>
+#include "nav2_msgs/srv/clear_entire_costmap.hpp"
 namespace real_robot_plugins
 {
 
-class LoadNewMap : public BT::SyncActionNode
+class LoadNewMap : public BT::StatefulActionNode
 {
 public:
     LoadNewMap(
@@ -21,12 +22,21 @@ public:
 
     static BT::PortsList providedPorts();
 
-    BT::NodeStatus tick() override;
+    BT::NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
 
 private:
+    rclcpp::Client<nav2_msgs::srv::ClearEntireCostmap>::SharedPtr global_costmap_client_;
+    rclcpp::Client<nav2_msgs::srv::ClearEntireCostmap>::SharedPtr local_costmap_client_;
     rclcpp::Node::SharedPtr node_;
     rclcpp::Client<nav2_msgs::srv::LoadMap>::SharedPtr map_client_;
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
+    rclcpp::Time start_time_;
+    double timeout_duration_=10.0; // seconds
+    std::string map_url_;
+    double x_, y_, yaw_;
+    std::shared_future<nav2_msgs::srv::LoadMap::Response::SharedPtr> future_result_;
 };
 
 }
--- real_robot_plugins/src/load_new_map.cpp
+++ real_robot_plugins/src/load_new_map.cpp
@@ -4,14 +4,21 @@
 namespace real_robot_plugins
 {
 
-LoadNewMap::LoadNewMap(
-    const std::string & name,
-    const BT::NodeConfiguration & config)
-    : BT::SyncActionNode(name, config)
+LoadNewMap::LoadNewMap(const std::string & name, const BT::NodeConfiguration & config)
+    : BT::StatefulActionNode(name, config) 
 {
-    node_ = rclcpp::Node::make_shared("load_map_action_node");
+    if (!config.blackboard->get<rclcpp::Node::SharedPtr>("node", node_)) {
+        node_ = rclcpp::Node::make_shared("load_map_fallback_node");
+    }
     map_client_ = node_->create_client<nav2_msgs::srv::LoadMap>("/map_server/load_map");
     pose_pub_ = node_->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 10);
+    global_costmap_client_ =
+    node_->create_client<nav2_msgs::srv::ClearEntireCostmap>(
+        "/global_costmap/clear_entirely_global_costmap");
+
+    local_costmap_client_ =
+        node_->create_client<nav2_msgs::srv::ClearEntireCostmap>(
+            "/local_costmap/clear_entirely_local_costmap");
 }
 
 BT::PortsList LoadNewMap::providedPorts()
@@ -23,46 +30,61 @@ BT::PortsList LoadNewMap::providedPorts()
         BT::InputPort<double>("yaw")
     };
 }
-
-BT::NodeStatus LoadNewMap::tick()
+BT::NodeStatus LoadNewMap::onStart()
 {
-    std::string map_url;
-    double x, y, yaw;
-
-    if (!getInput("map_url", map_url) || !getInput("x", x) || !getInput("y", y) || !getInput("yaw", yaw)) {
+    if (!getInput("map_url", map_url_) || !getInput("x", x_) || !getInput("y", y_) || !getInput("yaw", yaw_)) {
         return BT::NodeStatus::FAILURE;
     }
 
-    if (!map_client_->wait_for_service(std::chrono::seconds(2))) {
+    if (!map_client_->wait_for_service(std::chrono::seconds(1))) {
         return BT::NodeStatus::FAILURE;
     }
-
+    start_time_ = node_->get_clock()->now();
     auto request = std::make_shared<nav2_msgs::srv::LoadMap::Request>();
-    request->map_url = map_url;
+    request->map_url = map_url_;
+    future_result_ = map_client_->async_send_request(request).share();
+    
+    return BT::NodeStatus::RUNNING;
+}
+BT::NodeStatus LoadNewMap::onRunning()
+{
+    // rclcpp::spin_some(node_);
+    auto status = future_result_.wait_for(std::chrono::milliseconds(0));
+    if (status == std::future_status::ready) {
+        auto response = future_result_.get();
+        if (response->result != nav2_msgs::srv::LoadMap::Response::RESULT_SUCCESS) {
+            RCLCPP_ERROR(node_->get_logger(), "Load map failed");
+            return BT::NodeStatus::FAILURE;
+        }
+        auto clear_req = std::make_shared<nav2_msgs::srv::ClearEntireCostmap::Request>();
 
-    auto result = map_client_->async_send_request(request);
-    if (rclcpp::spin_until_future_complete(node_, result) != rclcpp::FutureReturnCode::SUCCESS) {
+        global_costmap_client_->async_send_request(clear_req);
+        local_costmap_client_->async_send_request(clear_req);
+        geometry_msgs::msg::PoseWithCovarianceStamped pose_msg;
+        pose_msg.header.frame_id = "map";
+        pose_msg.header.stamp = node_->get_clock()->now();
+        pose_msg.pose.pose.position.x = x_;
+        pose_msg.pose.pose.position.y = y_;
+        
+        tf2::Quaternion q;
+        q.setRPY(0, 0, yaw_);
+        pose_msg.pose.pose.orientation.x = q.x();
+        pose_msg.pose.pose.orientation.y = q.y();
+        pose_msg.pose.pose.orientation.z = q.z();
+        pose_msg.pose.pose.orientation.w = q.w();
+
+        pose_pub_->publish(pose_msg);
+        return BT::NodeStatus::SUCCESS;
+    }
+    if ((node_->get_clock()->now() - start_time_).seconds() > timeout_duration_) {
+        RCLCPP_ERROR(node_->get_logger(), "Load map timed out");
         return BT::NodeStatus::FAILURE;
     }
-
-    geometry_msgs::msg::PoseWithCovarianceStamped pose_msg;
-    pose_msg.header.frame_id = "map";
-    pose_msg.header.stamp = node_->get_clock()->now();
-    pose_msg.pose.pose.position.x = x;
-    pose_msg.pose.pose.position.y = y;
-    
-    tf2::Quaternion q;
-    q.setRPY(0, 0, yaw);
-    pose_msg.pose.pose.orientation.x = q.x();
-    pose_msg.pose.pose.orientation.y = q.y();
-    pose_msg.pose.pose.orientation.z = q.z();
-    pose_msg.pose.pose.orientation.w = q.w();
-
-    pose_pub_->publish(pose_msg);
-
-    return BT::NodeStatus::SUCCESS;
+    return BT::NodeStatus::RUNNING;
 }
-
+void LoadNewMap::onHalted()
+{
+}
 }
 
 BT_REGISTER_NODES(factory)
